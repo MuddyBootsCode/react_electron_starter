1eea68ccfc7039d73fd782a7fa53be1895292a6a
MuddyBootsCode <firsttogo98@gmail.com>
2017-11-22 08:54:29 -0600

    removed extraneous files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import store from './store';
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import  './index.css'
 
-render(<App store={store}/>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
